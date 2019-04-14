8a1281a46a99b1695c6daf03e83a4b15079d5050
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2019-04-14 15:01:32 -0700

    Todays Progress
    committing todays progress....

--- JesusWebsiteV2/js/HomePageScript.js
+++ JesusWebsiteV2/js/HomePageScript.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     $(window).scroll(function() {
       if($(this).scrollTop() > 200) { 
           $('.navbar').addClass('solid');
+          $(".nav-item").addClass("text-white");
+          $(".nav-item").removeClass("text-dark");
       } else {
           $('.navbar').removeClass('solid');
+          $(".nav-item").removeClass("text-white");
+          $(".nav-item").addClass("text-dark");
       }
     });
 });
