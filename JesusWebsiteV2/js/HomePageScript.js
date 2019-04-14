64180087d7af6f640e146121b25c657bff4fe0ea
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2019-04-14 14:27:09 -0700

    saving before redoing skills section
    Saving before redoing the section part, I don't like how it looks right now.

---
+++ JesusWebsiteV2/js/HomePageScript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 200) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
