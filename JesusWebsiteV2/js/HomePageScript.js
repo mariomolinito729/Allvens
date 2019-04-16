7f7efef51174a8d0920f4de8baadb097a369e4f7
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2019-04-16 02:38:59 -0700

    Added footer animation

--- JesusWebsiteV2/js/HomePageScript.js
+++ JesusWebsiteV2/js/HomePageScript.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
       $(".navbar").removeClass("solid");
     }
 
-    if ($(this).scrollTop() > 1000 && appCardsAnimated == false) {
+    if ($(this).scrollTop() > 900 && appCardsAnimated == false) {
       appCardsAnimated = true;
       $(function() {
         animate("#appCard", "zoomIn");
@@ -36,8 +36,6 @@ $(document).ready(function() {
   });
 });
 
-$('.navbar-nav').localScroll({duration:800});
-
 // $(".checkOutHolder").hover(function() {
 //     $(this).addClass("animated shake");
 //     var wait = setTimeout(function() {
@@ -45,7 +43,6 @@ $('.navbar-nav').localScroll({duration:800});
 //     }, 1000);
 //     console.log("lkadjsf");
 // });
-
 
 $(function() {
   $("#animateButton").click(function() {
@@ -61,3 +58,92 @@ $(function() {
     }, 1000);
   }
 });
+
+var footerCanvas = document.querySelector("#footerCanvas");
+
+var footerCanvasWidth = window.innerWidth;
+var footerCanvasHeight = 200;
+
+footerCanvas.width = footerCanvasWidth;
+footerCanvas.height = footerCanvasHeight;
+
+var footerC = footerCanvas.getContext("2d");
+
+function Circle(x, xv, y, yv, radius) {
+  this.x = x;
+  this.xVelocity = xv;
+  this.y = y;
+  this.yVelocity = yv;
+  this.radius = radius;
+
+  this.draw = function() {
+    footerC.beginPath();
+    footerC.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    footerC.fillStyle = "rgba(255, 255, 255, 0.514)";
+    footerC.fill();
+  };
+
+  this.update = function() {
+    console.log("This ran");
+    // update the circle to move
+    if (
+      this.x + this.radius > footerCanvasWidth - 10 ||
+      this.x - this.radius < 0
+    ) {
+      this.xVelocity = -this.xVelocity;
+    }
+    if (this.y + this.radius > footerCanvasHeight || this.y - this.radius < 0) {
+      // make it the opposite to make it go the opposite
+      //// direction
+      this.yVelocity = -this.yVelocity;
+    }
+
+    this.x += this.xVelocity;
+
+    this.y += this.yVelocity;
+
+    // after upadting the circle we are going to want
+    //// to show the changes on canvas
+    this.draw();
+  };
+}
+
+var circlesOnCanvas = [];
+
+for (var i = 0; i < 100; i++) {
+  var randCircleRadius = Math.random() * 20;
+
+  var randXCoord =
+    Math.random() * (window.innerWidth - randCircleRadius * 2) +
+    randCircleRadius;
+
+  var randXVelocity = (Math.random() - 0.5) * 8;
+
+  var randYCoord =
+    Math.random() * (window.innerHeight - randCircleRadius * 2) +
+    randCircleRadius;
+
+  var randYVelocity =(Math.random() - 0.5) * 8;
+
+  circlesOnCanvas.push(
+    new Circle(
+      randXCoord,
+      randYVelocity,
+      randYCoord,
+      randYVelocity,
+      randCircleRadius
+    )
+  );
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  footerC.clearRect(0, 0, footerCanvasWidth, footerCanvasHeight);
+
+  for (var item in circlesOnCanvas) {
+    circlesOnCanvas[item].update();
+  }
+}
+
+animate();
